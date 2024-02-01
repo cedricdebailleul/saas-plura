ece8fb18d715313ae9a8827588a8ee8000cf73cf
cedric debailleul <cedric.debailleul62@gmail.com>
2024-02-01 17:40:38 +0100

    📦 NEW: installation de nextjs, clerk, shadcn, uploadthing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "uplaodthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
